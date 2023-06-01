38dc0ae5925e03719c6bf2f83ef2c75cb9aaf1ca
immortal <ali.hatampouri@gmail.com>
2023-06-01 22:00:41 +0330

    Display the todo status message in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,24 @@
+import { useTodos } from "../Providers/TodosProvider";
+
 const Navbar = () => {
+  const todos = useTodos();
+
+  const todosCount = todos.length;
+  const completedCount = todos.filter((todo) => todo.isCompleted).length;
+
+  const doneMsg = () => {
+    if (todosCount) {
+      if (completedCount) {
+        if (todosCount === completedCount) {
+          return `Well done, you have done everything`;
+        }
+        return `Excellent, ${completedCount} of ${todosCount} tasks are done`;
+      }
+      return `Still ${todosCount} tasks left to do`;
+    }
+    return "Nothing to do yet!";
+  };
+
   return (
     <header className="backdrop-blur flex-none lg:border-b sticky top-0 z-50">
       <div className="max-w-8xl mx-auto">
@@ -7,7 +27,7 @@ const Navbar = () => {
             <h1 className="flex-none md:w-auto mr-3">ToDo list</h1>
             <div className="lg:flex ml-auto">
               <div className="cursor-default bg-slate-400/10 hover:bg-slate-400/20 items-center leading-5 px-3 py-1 rounded-full text-xs text-slate-800">
-                count
+                {doneMsg()}
               </div>
             </div>
           </div>
